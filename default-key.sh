1e5aca26b5cf633e0a6e37efefb9778a4820109b
Lambert Tran <lb.tran648@gmail.com>
2020-04-04 17:50:36 -0700

    fix variable name

--- default-key.sh
+++ default-key.sh
@@ -2,4 +2,4 @@
 
 PUBLIC_KEY="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHzW7NBs3dzfbR6fVbv4fvxLfn5IHa0KmZwaHYt5MMUi"
 
-echo ${PUBLUC_KEY} >> /home/vagrant/.ssh/authorized_keys
+echo ${PUBLIC_KEY} >> /home/vagrant/.ssh/authorized_keys
